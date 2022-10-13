09e02b58bca31cdb87736ebfbf5dfcff17c0d4cb
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-10-13 16:47:48 -0700

    finished story 3, added extras

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 animal_a = Animal.create(common_name: "Bird", scientific_binomial: "Birdius Birdulia")
+animal_b = Animal.create(common_name: "Hamster", scientific_binomial: "Hammonus Furrilius")
 
-sighting_a = Sighting.create(latitude: "40", longitude: "-73", date: "1999-05-05")
+sighting_a = Sighting.create(latitude: "-42", longitude: "-73", date: "1999-05-05")
+sighting_b = Sighting.create(latitude: "177.00484", longitude: "-42.9889", date: "2002-05-05")
 
 site_a = Site.create(animal: animal_a, sighting: sighting_a)
+site_b = Site.create(animal: animal_b, sighting: sighting_b)
